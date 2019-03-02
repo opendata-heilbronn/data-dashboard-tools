59a7fe40b01f69c55b8b12ed77e1a9aacc9028cc
Adrian Stabiszewski <as@grundid.de>
2019-03-02 20:08:52 +0100

    refactoring

--- trees/lib/geojson.dart
+++ trees/lib/geojson.dart
@@ -20,6 +20,33 @@ String getStadtteil(Map<String, dynamic> properties) {
   return properties["STADTTEIL"];
 }
 
+void writeTreeDatenwabe(
+    String prefix, String label, int trees, String textBottom,
+    {String backText}) {
+  Datenwabe datenwabe = Datenwabe.empty();
+
+  Front front = datenwabe.front;
+  front
+    ..textTop = "Es gibt über"
+    ..value = "$trees"
+    ..textBottom = textBottom
+    ..format = "int"
+    ..background = "img/tree.svg"
+    ..color = "#ffffff";
+
+  Back back = datenwabe.back;
+  back.text = backText ?? "Das Baumkataster hat den Datenstand von 2017";
+  back.color = "#000000";
+
+  File file = File("$prefix $label.json"
+      .replaceAll(" ", "-")
+      .toLowerCase()
+      .replaceAll("ö", "oe")
+      .replaceAll("ä", "ae")
+      .replaceAll("ü", "ue"));
+  file.writeAsStringSync(json.encode(datenwabe.toJson()));
+}
+
 void readGeoJson() async {
   File geojson = new File('baumkataster_022017.geojson');
   String data = await geojson.readAsString();
@@ -29,41 +56,39 @@ void readGeoJson() async {
       .toList();
 
   Map<String, List<Feature>> perStadtTeil = {};
+  Map<String, int> treeKindCounter = {};
   for (Feature feature in features) {
     String stadtTeil = getStadtteil(feature.properties);
     List<Feature> stadtTeilFeatures =
         perStadtTeil.putIfAbsent(stadtTeil, () => []);
     stadtTeilFeatures.add(feature);
-  }
 
-  List<Datenwabe> datenwaben = [];
+    String deuText = feature.properties["DEU_TEXT"];
+
+    int counter = treeKindCounter.putIfAbsent(deuText, () => 0);
+    treeKindCounter[deuText] = counter + 1;
+  }
 
   for (MapEntry<String, List<Feature>> mapEntry in perStadtTeil.entries) {
     if (mapEntry.key != null) {
       print("${mapEntry.key} => ${mapEntry.value.length}");
-
-      Datenwabe datenwabe = Datenwabe.empty();
-
-      Front front = datenwabe.front;
-      front.textTop = "Es gibt über";
-      front.value = "${mapEntry.value.length}";
-      front.textBottom = "in " + mapEntry.key;
-      front.format = "int";
-      front.background = "img/tree.svg";
-      front.color = "#ffffff";
-
-      Back back = datenwabe.back;
-      back.text = "Das Baumkataster hat den Datenstand von 2017";
-      back.color = "#000000";
-      datenwaben.add(datenwabe);
-
-      File file = File("trees ${mapEntry.key}.json"
-          .replaceAll(" ", "-")
-          .toLowerCase()
-          .replaceAll("ö", "oe")
-          .replaceAll("ä", "ae")
-          .replaceAll("ü", "ue"));
-      file.writeAsStringSync(json.encode(datenwabe.toJson()));
+      writeTreeDatenwabe("trees", mapEntry.key, mapEntry.value.length,
+          "Bäume in " + mapEntry.key);
     }
+  }
+
+  List<MapEntry<String, int>> treeKinds =
+      treeKindCounter.entries.where((entry) => entry.value > 1000).toList();
+  treeKinds.sort((k1, k2) => k1.value - k2.value);
+
+  int order = 1;
+  for (MapEntry<String, int> mapEntry in treeKinds) {
+    print("${mapEntry.key} => ${mapEntry.value}");
+    writeTreeDatenwabe(
+        "treekinds", mapEntry.key, mapEntry.value, mapEntry.key + " Bäume",
+        backText:
+            "${mapEntry.key} ist auf Rang $order bei der Baumart in Heilbronn");
+
+    order++;
   }
 }
